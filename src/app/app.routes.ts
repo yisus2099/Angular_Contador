436cbab60c89c25692d31df4db813d46bbcf6462
Darth_Yisus2099 <jesusalejandrot958@gmail.com>
2024-05-13 20:57:33 -0600

    Se agrega componente Info

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { EjemploComponent } from './ejemplo/ejemplo.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { InfoComponent } from './info/info.component';
 
 export const routes: Routes = [
     {path: '', component: InicioComponent},
     {path: 'inicio', component: InicioComponent},
     {path: 'ejemplito', component: EjemploComponent},
+    {path: 'info', component: InfoComponent},
 ];
 
 @NgModule({
